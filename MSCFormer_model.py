03130f36fdfbf9acd882dd9a048bed16189aa4e2
snailpt <zhaowei701@163.com>
2025-04-16 22:53:46 +0800

    Update MSCFormer_model.py

--- MSCFormer_model.py
+++ MSCFormer_model.py
@@ -11,36 +11,47 @@ import random
 import datetime
 import time
 import os
-from pandas import ExcelWriter
-from torchsummary import summary
-import torch
-from torch.backends import cudnn
-from utils import calMetrics
-from utils import calculatePerClass
-from utils import numberClassChannel
 import math
-
-cudnn.benchmark = False
-cudnn.deterministic = True
-
-
-
 import torch
+from torchsummary import summary
+from torch.backends import cudnn
 from torch import nn
 from torch import Tensor
 from einops.layers.torch import Rearrange, Reduce
 from einops import rearrange, reduce, repeat
 import torch.nn.functional as F
-
-from utils import numberClassChannel
-from utils import load_data_evaluate
-
-import numpy as np
-import pandas as pd
 from torch.autograd import Variable
+cudnn.benchmark = False
+cudnn.deterministic = True
 
+class Parameters():
+    def __init__(self, dropout_rate):
+        # The number of heads in the multi-head self-attention mechanism
+        self.heads = 8
+        # Transformer encoder depth
+        self.depth = 5
+        # The total number of feature channels of the multi-scale convolution module
+        self.emb_size = 16*3
+        # The number of feature channels at each time scale of the convolution module
+        self.f1 = 16 
+        # average pooling size in Convolution module, 44 for BCI IV-2a and 52 for BCI IV-2b
+        self.pooling_size = 52  
+        # drop out rate in Convolution module, 0.5 for subject-specific, and 0.25 for corss-subject
+        self.dropout_rate = dropout_rate
+
+def numberClassChannel(database_type):
+    if database_type=='A': # BCI IV-2a dataset
+        number_class = 4
+        number_channel = 22
+    elif database_type=='B': # BCI IV-2b dataset
+        number_class = 2
+        number_channel = 3
+    return number_class, number_channel
 
 class PatchEmbeddingCNN(nn.Module):
+    '''
+    Multi-scale convolutional module
+    '''
     def __init__(self, 
                  f1=16, 
                  pooling_size=52, 
@@ -81,7 +92,7 @@ class PatchEmbeddingCNN(nn.Module):
         x1 = self.cnn1(x)
         x2 = self.cnn2(x)
         x3 = self.cnn3(x)
-        #通道方向合并
+        # Concatenate along feature channel dimension.
         x = torch.cat([x1, x2, x3], dim=1)
         x = self.projection(x)
         return x    
@@ -117,7 +128,6 @@ class MultiHeadAttention(nn.Module):
         return out
 
 
-# PointWise FFN
 class FeedForwardBlock(nn.Sequential):
     def __init__(self, emb_size, expansion, drop_p):
         super().__init__(
@@ -155,7 +165,7 @@ class ResidualAdd(nn.Module):
         out = self.layernorm(self.drop(res)+x_input)
         return out
 
-    
+
 class TransformerEncoderBlock(nn.Sequential):
     def __init__(self,
                  emb_size,
@@ -178,18 +188,6 @@ class TransformerEncoder(nn.Sequential):
         super().__init__(*[TransformerEncoderBlock(emb_size, heads) for _ in range(depth)])
 
 
-class BranchEEGNetTransformer(nn.Sequential):
-    def __init__(self, parameters,
-                 **kwargs):
-        super().__init__(
-            PatchEmbeddingCNN(f1=parameters.f1, 
-                                 pooling_size=parameters.pooling_size, 
-                                 dropout_rate=parameters.dropout_rate,
-                                 number_channel=parameters.number_channel,
-                                 ),
-        )
-
-
 class PositioinalEncoding(nn.Module):
     def __init__(self, embedding, length=100, dropout=0.1):
         super().__init__()
@@ -204,13 +202,16 @@ class MSCFormer(nn.Module):
     def __init__(self, 
                  parameters,
                  database_type='A', 
-                 
                  **kwargs):
         super().__init__()
         self.number_class, self.number_channel = numberClassChannel(database_type)
         self.emb_size = parameters.emb_size
         parameters.number_channel = self.number_channel
-        self.cnn = BranchEEGNetTransformer(parameters)
+        self.cnn = PatchEmbeddingCNN(f1=parameters.f1, 
+                                 pooling_size=parameters.pooling_size, 
+                                 dropout_rate=parameters.dropout_rate,
+                                 number_channel=parameters.number_channel,
+                                 )
         self.position = PositioinalEncoding(parameters.emb_size, dropout=0.1)
         self.trans = TransformerEncoder(parameters.heads, 
                                         parameters.depth, 
@@ -219,12 +220,17 @@ class MSCFormer(nn.Module):
         self.flatten = nn.Flatten()
         self.classification = ClassificationHead(self.emb_size , self.number_class) 
     def forward(self, x):
+        # x.shape = [batch, feature channels, electrodes channels, length], such as [batch, 1, 22, 1000] in BCI IV-2a dataset.
         x = self.cnn(x)
         b, l, e = x.shape
+        
+        # Add class token like BERT
         x = torch.cat((torch.zeros((b, 1, e),requires_grad=True).cuda(),x), 1)
         x = x * math.sqrt(self.emb_size)
         x = self.position(x)
         trans = self.trans(x)
+
+        # Take the class token as the final features for classifying.
         features = trans[:, 0, :]
         
         out = self.classification(features)
